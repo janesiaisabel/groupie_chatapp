056bea7003f96c794f6688ee29ed849a413bb954
Janes <janesiaisabel19@gmail.com>
2023-08-16 02:14:17 +0700

    Profile Page UI

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -94,7 +94,8 @@ class _HomePageState extends State<HomePage> {
             ),
             ListTile(
               onTap: () {
-                nextScreen(context, const ProfilePage());
+                nextScreenReplace(
+                    context, ProfilePage(userName: userName, email: email));
               },
               contentPadding:
                   const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
